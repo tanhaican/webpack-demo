be5604406ae7979dfc66be01ff0b663cc6705ae4
tanhaican <tanhaican@126.com>
2017-02-06 15:28:03 +0800

    提交代码

---
+++ demo05/readme.md
@@ -0,0 +1,8 @@
+通过执行
+npm start
+或者
+npm run start
+启动webpack编译
+
+
+参考教程：https://vikingmute.gitbooks.io/webpack-for-fools/content/entries/chapter-1.html
---
+++ demo05/webpack.config.dev.js
@@ -0,0 +1,43 @@
+var merge = require( 'webpack-merge' );
+var HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+var BASE_CONFIG = require( './webpack.config' );
+var path = require('path');
+//定义了一些文件夹的路径
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = merge(BASE_CONFIG, {
+    devtool: 'eval-source-map', //配置生成Source Maps，选择合适的选项
+    //输出的文件名 合并以后的js会命名为bundle.js
+    output: {
+        path: './',
+        //注意 我们修改了bundle.js 用一个数组[name]来代替，
+        // 他会根据entry的入口文件名称生成多个js文件，
+        // 这里就是(app.js, mobile.js和vendors.js)
+        filename: '[name].[hash].js'
+    },
+    module: {
+        preLoaders: [
+            {test: /\.jsx?$/, include: APP_PATH, loader: 'jshint-loader'}
+        ]
+    },
+    //配置jshint的选项，支持es6的校验
+    jshint: {
+        "esnext": true
+    },
+    //添加我们的插件 会自动生成一个html文件
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(ROOT_PATH, 'demo') + '/index.html',
+            inject: 'head'
+        })
+    ],
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true
+    }
+} );
